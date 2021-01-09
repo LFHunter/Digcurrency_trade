69501c360bd2802cdfa866ac3d6ee323ffdaa352
Leosporting <luck31101@hotmail.com.tw>
2021-01-10 02:58:08 +0800

    Add  BinanceExchange:get_orderbook function , FindSpread:find_Binance_cheapest_price

--- FindSpread.py
+++ FindSpread.py
@@ -1,5 +1,6 @@
 from  DigExchangeLib.BitoproExchange import BitoproExchange
 from  DigExchangeLib.MaxExchange import MaxExchange
+from  DigExchangeLib.BinanceExchange import BinanceExchange
 """
 
 'bids': [{'amount': '0.00657471',
@@ -12,12 +13,12 @@ from  DigExchangeLib.MaxExchange import MaxExchange
            'price': '38723.41',
            'total': '0.0016'}}
 """
-def  find_Bito_cheapest_price(asksList):
+def  find_Bito_cheapest_price(bito_orderbook):
+    asksList = bito_orderbook['asks']
     cheapestinfo = {"price":asksList[0]['price'],'amount':asksList[0]['amount']}
     for  ask  in asksList:
         if  float(ask['price']) < float(cheapestinfo['price']) :
             cheapestinfo = ask
-    print(cheapestinfo)
     return  cheapestinfo
 
 
@@ -27,7 +28,20 @@ def  find_Max_cheapest_price(maxE_orderbook):
     for  order in maxE_orderbook:
         if  order['side'] == "ask"  and  float(cheapestinfo['price'])  < float(order['price']):
             cheapestinfo =  {"price": float(order['price']) , 'amount':order['volume']}
-    print(cheapestinfo)
+    return cheapestinfo
+
+def find_Binance_cheapest_price(binance_orderbook):
+    """
+            {'lastUpdateId': 4725681499,
+             'bids': [['1226.05000000', '30.00000000'], ['1225.84000000', '1.65491000'], ['1225.82000000', '5.20000000'],
+                      ['1225.79000000', '0.16309000'], ['1225.78000000', '5.14245000']],
+             'asks': [['1226.28000000', '1.56131000'], ['1226.29000000', '32.80000000'], ['1226.30000000', '32.81681000'],
+                      ['1226.31000000', '0.13522000'], ['1226.32000000', '0.58248000']]}
+            """
+    cheapestinfo = {"price": binance_orderbook['asks'][0][0], 'amount': binance_orderbook['asks'][0][1]}
+    for order in binance_orderbook['asks']:
+        if float(cheapestinfo['price']) < float(order[0]):
+            cheapestinfo = {"price": float(order[0]), 'amount': order[1]}
     return cheapestinfo
 
 """Max:
@@ -39,9 +53,14 @@ btc/eth/ltc/bch/mith/xrp/max/sand       usdt
 
 maxE = MaxExchange()
 maxE_orderbook = maxE.get_orderbook("ethusdt",limit=4)
-find_Max_cheapest_price(maxE_orderbook)
+max_price = find_Max_cheapest_price(maxE_orderbook)
 bito = BitoproExchange()
 bito_orderbook = bito.get_orderbook("eth_usdt")
-find_Bito_cheapest_price(bito_orderbook['asks'])
+bito_price = find_Bito_cheapest_price(bito_orderbook)
 
+binance = BinanceExchange()
+binance_orderbook =  binance .get_orderbook("ETHUSDT")
+binance_price  = find_Binance_cheapest_price(binance_orderbook)
+
+print(f"Max:{max_price}\nBito:{bito_price}\nBinance:{binance_price}")
 
